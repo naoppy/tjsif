2c4a7b12e8795955be56900d0f86ed74b061f043
Otani Naoki <e1710@s.akashi.ac.jp>
2019-07-01 22:52:50 +0900

    mv main.py edgetpu_demo.py create main.py add new file (video_writer_helper.py)

--- main.py
+++ main.py
@@ -1,65 +1,8 @@
-import argparse
+# coding=utf-8
 import cv2
-import imp
-import numpy as np
-import os
-import re
-import time
-from PIL import Image
-from edgetpu.detection.engine import DetectionEngine
 
 
-def load_labels(path):
-    p = re.compile(r'\s*(\d+)(.+)')
-    with open(path, 'r', encoding='utf-8') as f:
-        lines = (p.match(line).groups() for line in f.readlines())
-        return {int(num): text.strip() for num, text in lines}
-
-
-def get_person_list(objs, labels):
-    return_list = []
-    for obj in objs:
-        x0, y0, x1, y1 = obj.bounding_box.flatten().tolist()
-        percent = int(100 * obj.score)
-        if labels[obj.label_id] == "person":
-            return_list.append((x0, y0, x1, y1, percent))
-
-    return return_list
-
-
-def write_rect(img, tuple):
-    height, width, _ = img.shape
-    x0, y0, x1, y1, percent = tuple
-    x0, y0, x1, y1 = int(x0 * width), int(y0 * height), int(x1 * width), int(y1 * height)
-    cv2.rectangle(img, (x0, y0), (x1, y1), (0, 0, 255), 2)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img, '%d%%' % percent, (x0, y0-10), font, 0.6, (0, 0, 255), 1, cv2.LINE_AA)
-
-
-def prepare_edgetpu():
-    default_model_dir = './all_models'
-    default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
-    default_labels = 'coco_labels.txt'
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', help='.tflite model path',
-                        default=os.path.join(default_model_dir, default_model))
-    parser.add_argument('--labels', help='label file path',
-                        default=os.path.join(default_model_dir, default_labels))
-    parser.add_argument('--top_k', type=int, default=10,
-                        help='number of classes with highest score to display')
-    parser.add_argument('--threshold', type=float, default=0.2,
-                        help='class score threshold')
-    args = parser.parse_args()
-
-    print("Loading %s with %s labels." % (args.model, args.labels))
-    engine = DetectionEngine(args.model)
-    labels = load_labels(args.labels)
-    threshold = args.threshold
-    top_k = args.top_k
-    return engine, labels, threshold, top_k,
-
-
-def main_loop():
+def main():
     cap = cv2.VideoCapture(0)
 
     height = int(cap.get(3))
@@ -74,7 +17,7 @@ def main_loop():
     engine, labels, threshold, top_k = prepare_edgetpu()
 
     # main func
-    def main_func(image):
+    def edge_detect(image):
         rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         im_pil = Image.fromarray(rgb)
 
@@ -83,12 +26,12 @@ def main_loop():
                                       keep_aspect_ratio=True, relative_coord=True,
                                       top_k=top_k)
         end_time = time.monotonic()
-        text_lines = [
-            'Inference: %.2f ms' % ((end_time - start_time) * 1000),
-            'FPS: %.2f fps' % (1.0 / (end_time - start_time)),
-            '%d object found' % len(objs),
-        ]
-        #print(' '.join(text_lines))
+        # text_lines = [
+        #     'Inference: %.2f ms' % ((end_time - start_time) * 1000),
+        #     'FPS: %.2f fps' % (1.0 / (end_time - start_time)),
+        #     '%d object found' % len(objs),
+        # ]
+        # print(' '.join(text_lines))
         person_list = get_person_list(objs, labels)
         for e in person_list:
             write_rect(image, e)
@@ -97,7 +40,7 @@ def main_loop():
     while True:
         ret, frame = cap.read()
 
-        processed_frame = main_func(frame)
+        processed_frame = edge_detect(frame)
 
         cv2.imshow('processed_frame', processed_frame)
         out.write(processed_frame)
@@ -111,4 +54,4 @@ def main_loop():
 
 
 if __name__ == '__main__':
-    main_loop()
+    main()
