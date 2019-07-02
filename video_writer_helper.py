58679788ec8589321a2a62447e27e6d2fb286849
Otani Naoki <e1710@s.akashi.ac.jp>
2019-07-02 21:15:44 +0900

    impl VideoWriterHelper

--- video_writer_helper.py
+++ video_writer_helper.py
@@ -3,7 +3,7 @@ import time
 
 class VideoWriteHelper:
     def __init__(self, fps, codec='X264'):
-        self.lasttime = None
+        self.last_time = None
         self.fps = fps
         self.sec_par_frame = 1.0 / fps
         fourcc = cv2.VideoWriter_fourcc(*codec)
@@ -11,11 +11,15 @@ class VideoWriteHelper:
         self.writer = out
 
     def write_frame(self, frame):
-        if self.lasttime is None:
-            # TODO!!!!!!
-            pass
+        if self.last_time is None:
+            self.writer.write(frame)
+        else:
+            time_diff = time.monotonic() - self.last_time
+            frame_times = round(time_diff / self.sec_par_frame)
+            for _ in range(frame_times):
+                self.writer.write(frame)
 
-        time_diff = self.lasttime - time.monotonic()
-        time = time_diff / self.sec_par_frame
+        self.last_time = time.monotonic()
 
-        self.lasttime = time.monotonic()
+    def release(self):
+        self.writer.release()
