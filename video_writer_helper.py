ff590e49d8968c29b66950f99af245f0ef181c69
Otani Naoki <e1710@s.akashi.ac.jp>
2019-07-02 21:27:55 +0900

    bug fixed <width and height is not defined>

--- video_writer_helper.py
+++ video_writer_helper.py
@@ -1,8 +1,15 @@
+# coding=utf-8
 import time
 
 
 class VideoWriteHelper:
-    def __init__(self, fps, codec='X264'):
+    def __init__(self, fps, height, width, codec='X264'):
+        """
+        :param fps: 動画のfps
+        :param height: 動画の縦のサイズ
+        :param width: 動画の横サイズ
+        :param codec: ビデオコーデック, opencvのドキュメントを参照
+        """
         self.last_time = None
         self.fps = fps
         self.sec_par_frame = 1.0 / fps
@@ -11,6 +18,10 @@ class VideoWriteHelper:
         self.writer = out
 
     def write_frame(self, frame):
+        """
+        動画に画像を数フレーム計算して書き出す
+        :param frame: opencv形式の画像
+        """
         if self.last_time is None:
             self.writer.write(frame)
         else:
@@ -20,6 +31,11 @@ class VideoWriteHelper:
                 self.writer.write(frame)
 
         self.last_time = time.monotonic()
+        return
 
     def release(self):
+        """
+        後処理、これをしないとバグる
+        """
         self.writer.release()
+        return
