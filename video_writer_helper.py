bf3a213748dd141c3bb925976c3eebbef527acb6
Otani Naoki <e1710@s.akashi.ac.jp>
2019-12-14 00:30:31 +0900

    Fix Important Bug : Width and Height are opposite

--- video_writer_helper.py
+++ video_writer_helper.py
@@ -15,7 +15,7 @@ class VideoWriteHelper:
         self.fps = fps
         self.sec_par_frame = 1.0 / fps
         fourcc = cv2.VideoWriter_fourcc(*codec)
-        out = cv2.VideoWriter('output.avi', fourcc, fps, (height, width))
+        out = cv2.VideoWriter('output.avi', fourcc, fps, (width, height))
         self.writer = out
 
     def write_frame(self, frame):
