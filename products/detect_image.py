cff4b7d139a3f16ca527a884bd37d49d16015ed5
Otani Naoki <e1710@s.akashi.ac.jp>
2019-12-14 13:51:22 +0900

    Copyed rapi4/detect to products/detect

---
+++ products/detect_image.py
@@ -0,0 +1,70 @@
+# python3
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Example using TF Lite to detect objects in a given image."""
+
+import tflite_runtime.interpreter as tflite
+
+EDGETPU_SHARED_LIB = 'libedgetpu.so.1'
+
+
+def load_labels(path, encoding='utf-8'):
+    """Loads labels from file (with or without index numbers).
+
+    Args:
+      path: path to label file.
+      encoding: label file encoding.
+    Returns:
+      Dictionary mapping indices to labels.
+    """
+    with open(path, 'r', encoding=encoding) as f:
+        lines = f.readlines()
+        if not lines:
+            return {}
+
+        if lines[0].split(' ', maxsplit=1)[0].isdigit():
+            pairs = [line.split(' ', maxsplit=1) for line in lines]
+            return {int(index): label.strip() for index, label in pairs}
+        else:
+            return {index: line.strip() for index, line in enumerate(lines)}
+
+
+def make_interpreter(model_file):
+    """
+    Return EdgeTPU interpreter
+    :param model_file:
+        model file path
+        example: "path/to/model/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite"
+        Make sure you through path to file
+    """
+    model_file, *device = model_file.split('@')
+    return tflite.Interpreter(
+        model_path=model_file,
+        experimental_delegates=[
+            tflite.load_delegate(EDGETPU_SHARED_LIB,
+                                 {'device': device[0]} if device else {})
+        ])
+
+
+def draw_objects(draw, objs, labels):
+    """
+    Draws the bounding box and label for each object.
+    """
+    for obj in objs:
+        bbox = obj.bbox
+        draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)],
+                       outline='red')
+        draw.text((bbox.xmin + 10, bbox.ymin + 10),
+                  '%s\n%.2f' % (labels.get(obj.id, obj.id), obj.score),
+                  fill='red')
