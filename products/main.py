027d21a6e5ab2cb6614561a59049f254e3cb180e
Otani Naoki <e1710@s.akashi.ac.jp>
2019-12-14 22:54:43 +0900

    change main.py
    - use import raspi4/~ to import products/~
    - change edge_detect function
    - add decode_fourcc function

--- products/main.py
+++ products/main.py
@@ -2,9 +2,10 @@ import cv2
 import numpy as np
 from PIL import Image, ImageDraw
 
-import video_writer_helper as writer
-from raspi4 import detect_image, detect
+from products import detect_image, detect, motion_detect, video_writer_helper
 
+
+# Implementation of tjsif_flowchart.svg
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -12,11 +13,13 @@ def main():
     cap.set(cv2.CAP_PROP_FPS, 15)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
 
     print("fps:%d" % (cap.get(cv2.CAP_PROP_FPS)))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     print("Camera Height:%d Width:%d" % (height, width))
+    print("Camera Encoding:%s", decode_fourcc(cap.get(cv2.CAP_PROP_FOURCC)))
 
     model_file = "../all_models/mobilenet_ssd_v2_coco_quant_postprocess.tflite"
     label_file = "../all_models/coco_labels.txt"
@@ -29,10 +32,17 @@ def main():
     while True:
         ret, frame = cap.read()
 
-        
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        if motion_detect.frame_diff_detection(frame):
+            persons = edge_detect_person(interpreter, frame, threshold, labels)
+            if persons:
+                # There are moving persons
+                # start Recording Loop
+                pass
+            else:
+                pass
+        else:
+            # Do Nothing
+            pass
 
     cap.release()
     cv2.destroyAllWindows()
@@ -60,14 +70,22 @@ def edge_detect_person(interpreter, frame_cv, threshold, labels):
     objs = detect.get_output(interpreter, threshold, scale)
     persons = [x for x in objs if x.id == 0]
 
-    for obj in objs:
-        print('person')
-        print('  score: ', obj.score)
-        print('  bbox:  ', obj.bbox)
+    # THIS IS FOR DEBUG
+    # for obj in objs:
+    #     print('person')
+    #     print('  score: ', obj.score)
+    #     print('  bbox:  ', obj.bbox)
+    #
+    # detect_image.draw_objects(ImageDraw.Draw(image), persons, labels)
+    # cv_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    # return cv_image
+    return persons
 
-    detect_image.draw_objects(ImageDraw.Draw(image), persons, labels)
-    cv_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    return cv_image
+
+def decode_fourcc(v):
+    # https://amdkkj.blogspot.com/2017/06/opencv-python-for-windows-playing-videos_17.html
+    v = int(v)
+    return "".join([chr((v >> 8 * i) & 0xFF) for i in range(4)])
 
 
 if __name__ == '__main__':
