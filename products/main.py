47de592c104504b3bdf2f07b6ea48baddbf0237c
Otani Naoki <e1710@s.akashi.ac.jp>
2019-12-14 13:51:34 +0900

    Copyed main to products/

---
+++ products/main.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+from PIL import Image, ImageDraw
+
+import video_writer_helper as writer
+from raspi4 import detect_image, detect
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    cap.set(cv2.CAP_PROP_FPS, 15)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+
+    print("fps:%d" % (cap.get(cv2.CAP_PROP_FPS)))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print("Camera Height:%d Width:%d" % (height, width))
+
+    out = writer.VideoWriteHelper(15.0, height, width, "output.mov", "mp4v")
+
+    model_file = "../all_models/mobilenet_ssd_v2_coco_quant_postprocess.tflite"
+    label_file = "../all_models/coco_labels.txt"
+    threshold = 0.6
+
+    labels = detect_image.load_labels(label_file)
+    interpreter = detect_image.make_interpreter(model_file)
+    interpreter.allocate_tensors()
+
+    while True:
+        ret, frame = cap.read()
+
+        processed_frame = edge_detect(interpreter, frame, threshold, labels)
+
+        cv2.imshow('processed_frame', processed_frame)
+        out.write_frame(processed_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+def edge_detect(interpreter, frame_cv, threshold, labels):
+    """
+    object-detection using edge tpu
+    :param labels:
+    :param threshold:
+    :param interpreter:
+    :param frame_cv: OpenCV image
+    :return:
+    """
+    # OpenCVはBGR、PillowはRGB
+    frame_rgb = cv2.cvtColor(frame_cv, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(frame_rgb).convert("RGB")
+
+    scale = detect.set_input(interpreter, image.size,
+                             lambda size: image.resize(size, Image.ANTIALIAS))
+
+    interpreter.invoke()
+
+    objs = detect.get_output(interpreter, threshold, scale)
+
+    print('-------RESULTS--------')
+    if not objs:
+        print('No objects detected')
+
+    for obj in objs:
+        print(labels.get(obj.id, obj.id))
+        print('  id:    ', obj.id)
+        print('  score: ', obj.score)
+        print('  bbox:  ', obj.bbox)
+
+    detect_image.draw_objects(ImageDraw.Draw(image), objs, labels)
+    cv_bgr = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    return cv_bgr
+
+
+if __name__ == '__main__':
+    main()
