2374603aa278eea176c8b6970d96c063af64f3cc
Otani Naoki <e1710@s.akashi.ac.jp>
2019-12-15 22:54:08 +0900

    Change calc_rotation to prevent left and right round trip.

--- products/calc_rotation.py
+++ products/calc_rotation.py
@@ -1,20 +1,32 @@
 from motor import control_stepping_motor as motorCtl
+import time
+
+last_move_time = time.time()
+last_move_direction = "L"
 
 
 def rotate(persons, width):
-    # mean = statistics.mean([(person.bbox.xmax - person.bbox.min) / 2 for person in persons])
-    # if mean > width / 2:
-    #     # move camera right
-    #     motorCtl.right_spin_7_2degree()
-    # else:
-    #     # move camera left
-    #     motorCtl.left_spin_7_2degree()
-    minX = min([person.bbox.xmin for person in persons])
-    maxX = min([person.bbox.xmax for person in persons])
-    left_diff = minX
-    right_diff = width - maxX
+    if not persons:
+        return
+
+    global last_move_time
+    now = time.time()
+
+    min_x = min([person.bbox.xmin for person in persons])
+    max_x = min([person.bbox.xmax for person in persons])
+    left_diff = min_x
+    right_diff = width - max_x
+
+    move_direction = "R" if left_diff > right_diff else "L"
+
+    # Reduce left and right round trip
+    if now - last_move_time < 2 and last_move_direction != move_direction:
+        return
+
     if left_diff > right_diff:
         # rotate right
+        last_move_time = now
         motorCtl.right_spin_7_2degree()
     else:
+        last_move_time = now
         motorCtl.left_spin_7_2degree()
