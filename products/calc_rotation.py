d875a12eba60997af66298341b4f6692b8cf8664
Otani Naoki <e1710@s.akashi.ac.jp>
2019-12-17 00:29:03 +0900

    update prpducts/calc_rotation.py

--- products/calc_rotation.py
+++ products/calc_rotation.py
@@ -12,21 +12,31 @@ def rotate(persons, width):
     global last_move_time
     now = time.time()
 
+    if now - last_move_time < 2:
+        return
+
     min_x = min([person.bbox.xmin for person in persons])
     max_x = min([person.bbox.xmax for person in persons])
     left_diff = min_x
     right_diff = width - max_x
 
+    if left_diff == right_diff:
+        return
+
     move_direction = "R" if left_diff > right_diff else "L"
 
     # Reduce left and right round trip
-    if now - last_move_time < 2 and last_move_direction != move_direction:
+    global last_move_direction
+    if now - last_move_time < 3 and last_move_direction != move_direction:
         return
 
-    if left_diff > right_diff:
+    if move_direction == "R":
         # rotate right
         last_move_time = now
+        last_move_direction = "R"
         motorCtl.right_spin_7_2degree()
     else:
+        # rotate left
         last_move_time = now
+        last_move_direction = "L"
         motorCtl.left_spin_7_2degree()
